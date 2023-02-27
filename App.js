67a2fae0a8432c00898b4c55826797a266799891
Nadeem <anwarnadeem2010@gmail.com>
2023-02-27 23:15:44 +0530

    ignite our app finish

--- App.js
+++ App.js
@@ -1,26 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 const heading = React.createElement(
-    "h1",
-    { id: "title" },
-    // { id: "title", className: "titleClass" },
-    // { id: "title", style: { color: "red" } },
-    "Namaste React by react"
-  );
-  const heading1 = React.createElement(
-    "h1",
-    { id: "heading1" },
-    "heading 1"
-  );
-  const heading2 = React.createElement(
-    "h2",
-    { id: "heading2" },
-    "Heading 2"
-  );
-  const container = React.createElement("div", { id: "container" }, [
-    heading1,
-    heading2,
-  ]);
-  const root = ReactDOM.createRoot(document.getElementById("root"));
-  //   console.log("roor", root);
-  root.render(container);
+  "h1",
+  { id: "title" },
 
-  
+  "Namaste React by react"
+);
+const heading1 = React.createElement(
+  "h1",
+  { id: "heading1 " },
+  "heading 1 from dev"
+);
+const heading2 = React.createElement(
+  "h2",
+  { id: "heading2" },
+  "Heading 2 from dev"
+);
+const container = React.createElement("div", { id: "container" }, [
+  heading1,
+  heading2,
+]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(container);
