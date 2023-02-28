456a7e27fe080c215f63ff0bd069bcb3faa7736c
Nadeem <anwarnadeem2010@gmail.com>
2023-02-28 14:54:03 +0530

    remove reactCreateElement using JSx

--- App.js
+++ App.js
@@ -1,24 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-const heading = React.createElement(
-  "h1",
-  { id: "title" },
 
-  "Namaste React by react"
+
+// const heading = React.createElement(
+//   "h1",
+//   { id: "title" },
+
+//   "Namaste React by react"
+// );
+
+console.log(heading);
+
+// JSX
+
+const heading1 = (
+  <h1 id="heading1" key="head1">
+    Heading 1
+  </h1>
 );
-const heading1 = React.createElement(
-  "h1",
-  { id: "heading1 " },
-  "heading 1 from dev"
-);
-const heading2 = React.createElement(
-  "h2",
-  { id: "heading2" },
-  "Heading 2 from dev"
-);
-const container = React.createElement("div", { id: "container" }, [
-  heading1,
-  heading2,
-]);
+
+// const heading1 = React.createElement(
+//   "h1",
+//   { id: "heading1 ", key: "head1" },
+//   "heading 1 from dev"
+// );
+// const heading2 = React.createElement(
+//   "h2",
+//   { id: "heading2", key: "head2" },
+//   "Heading 2 from dev"
+// );
+// const container = React.createElement("div", { id: "container" }, [
+//   heading1,
+//   heading2,
+// ]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(container);
+root.render(heading1);
