76426723355deb868f1254a288ded9f266b72709
Nadeem <anwarnadeem2010@gmail.com>
2023-03-01 12:55:50 +0530

    cleaning app.js before coding

--- App.js
+++ App.js
@@ -1,17 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// const heading = React.createElement(
-//   "h1",
-//   { id: "title" },
-
-//   "Namaste React by react"
-// );
-
-// console.log(heading);
-
-// JSX
-
 const heading1 = (
   <h1 id="heading1" key="head1">
     This is react element
@@ -28,11 +17,6 @@ const Title = () => {
 const HeaderComponent = () => {
   return (
     <div>
-      {/* use react element inside component */}
-      {heading1}
-
-      {/* use react component inside component 2 way to do this */}
-      {/* {Title()} */}
       <Title />
       <h1>This is react functional component</h1>
       <h2>This is second header</h2>
@@ -41,11 +25,5 @@ const HeaderComponent = () => {
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-// for react element
-// root.render(heading1);
 
-//for react component
 root.render(<HeaderComponent />);
-
-// this will also work
-// root.render(HeaderComponent());
