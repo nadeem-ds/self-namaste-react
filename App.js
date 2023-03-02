6ad9e6676a5da064f4f7d157de81a2015857108a
Nadeem <anwarnadeem2010@gmail.com>
2023-03-02 22:56:22 +0530

    start props

--- App.js
+++ App.js
@@ -56,20 +56,417 @@ const HeaderComponent = () => {
   );
 };
 
-const restrauntData = {
-  img: "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/bdcd233971b7c81bf77e1fa4471280eb",
-  name: "Burger King",
-  cousinName: ["Cousins", "American"],
-  rating: "4.3",
-};
+const restrauntData = [
+  //   {
+  //   img: "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/bdcd233971b7c81bf77e1fa4471280eb",
+  //   name: "Burger King",
+  //   cousinName: ["Cousins", "American"],
+  //   rating: "4.3",
+  // },
+  //   {
+  //   img: "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/bdcd233971b7c81bf77e1fa4471280eb",
+  //   name: "Burger King",
+  //   cousinName: ["Cousins", "American"],
+  //   rating: "4.3",
+  // },
+  //   {
+  //   img: "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/bdcd233971b7c81bf77e1fa4471280eb",
+  //   name: "Burger King",
+  //   cousinName: ["Cousins", "American"],
+  //   rating: "4.3",
+  // },
+  //   {
+  //   img: "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/bdcd233971b7c81bf77e1fa4471280eb",
+  //   name: "Burger King",
+  //   cousinName: ["Cousins", "American"],
+  //   rating: "4.3",
+  // },
+  //   {
+  //   img: "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/bdcd233971b7c81bf77e1fa4471280eb",
+  //   name: "Burger King",
+  //   cousinName: ["Cousins", "American"],
+  //   rating: "4.3",
+  // },
 
-const RestrauntCard = () => {
+  {
+    type: "restaurant",
+    data: {
+      type: "F",
+      id: "79019",
+      name: "Pizza Hut",
+      uuid: "22bd3cc9-fcb4-4371-bb9b-f770d68e77e0",
+      city: "55",
+      area: "Indrapuri",
+      totalRatingsString: "1000+ ratings",
+      cloudinaryImageId: "jdpuywd9wmimqxbkzrsb",
+      cuisines: ["Pizzas"],
+      tags: [],
+      costForTwo: 35000,
+      costForTwoString: "₹350 FOR TWO",
+      deliveryTime: 37,
+      minDeliveryTime: 37,
+      maxDeliveryTime: 37,
+      slaString: "37 MINS",
+      lastMileTravel: 5,
+      slugs: {
+        restaurant: "pizza-hut-indrapuri-test-bhopal",
+        city: "bhopal",
+      },
+      cityState: "55",
+      address:
+        "Unit no. 2B/1, Opposite BHEL Jublie Gate, Indrapuri, Bhopal, Madhya Pradesh Pin-462021",
+      locality: "Opposite BHEL Jublie Gate",
+      parentId: 721,
+      unserviceable: false,
+      veg: false,
+      select: false,
+      favorite: false,
+      tradeCampaignHeaders: [],
+      aggregatedDiscountInfo: {
+        header: "FREE DELIVERY",
+        shortDescriptionList: [
+          {
+            meta: "FREE DELIVERY",
+            discountType: "FREE_DELIVERY",
+            operationType: "RESTAURANT",
+          },
+        ],
+        descriptionList: [
+          {
+            meta: "FREE DELIVERY",
+            discountType: "FREE_DELIVERY",
+            operationType: "RESTAURANT",
+          },
+        ],
+        subHeader: "",
+        headerType: 0,
+        superFreedel: "",
+      },
+      aggregatedDiscountInfoV2: {
+        header: "",
+        shortDescriptionList: [
+          {
+            meta: "Free Delivery",
+            discountType: "FREE_DELIVERY",
+            operationType: "RESTAURANT",
+          },
+        ],
+        descriptionList: [
+          {
+            meta: "FREE DELIVERY",
+            discountType: "FREE_DELIVERY",
+            operationType: "RESTAURANT",
+          },
+        ],
+        subHeader: "",
+        headerType: 0,
+        superFreedel: "",
+      },
+      ribbon: [
+        {
+          type: "PROMOTED",
+        },
+      ],
+      chain: [],
+      feeDetails: {
+        fees: [],
+        totalFees: 0,
+        message: "",
+        title: "",
+        amount: "",
+        icon: "",
+      },
+      availability: {
+        opened: true,
+        nextOpenMessage: "",
+        nextCloseMessage: "",
+      },
+      longDistanceEnabled: 0,
+      rainMode: "NONE",
+      thirdPartyAddress: false,
+      thirdPartyVendor: "",
+      adTrackingID: "cid=6111422~p=1~eid=00000186-a341-0f33-05ea-e8cc00ae0131",
+      badges: {
+        imageBased: [],
+        textBased: [],
+        textExtendedBadges: [],
+      },
+      lastMileTravelString: "5 kms",
+      hasSurge: false,
+      sla: {
+        restaurantId: "79019",
+        deliveryTime: 37,
+        minDeliveryTime: 37,
+        maxDeliveryTime: 37,
+        lastMileTravel: 5,
+        lastMileDistance: 0,
+        serviceability: "SERVICEABLE",
+        rainMode: "NONE",
+        longDistance: "NOT_LONG_DISTANCE",
+        preferentialService: false,
+        iconType: "EMPTY",
+      },
+      promoted: true,
+      avgRating: "3.8",
+      totalRatings: 1000,
+      new: false,
+    },
+    subtype: "basic",
+  },
+  {
+    type: "restaurant",
+    data: {
+      type: "F",
+      id: "109939",
+      name: "Zam Zam Fast Food",
+      uuid: "6bc8e735-16e5-4263-8017-672d85a8085d",
+      city: "55",
+      area: "Hamidia Road",
+      totalRatingsString: "10000+ ratings",
+      cloudinaryImageId: "kuisp5xx9tcutesp33yf",
+      cuisines: ["North Indian", "Chinese", "Indian", "Snacks"],
+      tags: [],
+      costForTwo: 20000,
+      costForTwoString: "₹200 FOR TWO",
+      deliveryTime: 24,
+      minDeliveryTime: 24,
+      maxDeliveryTime: 24,
+      slaString: "24 MINS",
+      lastMileTravel: 2.200000047683716,
+      slugs: {
+        restaurant:
+          "zam-zam-fast-food-old-bhopal-jahangirabad-old-bhopal-jahangirabad",
+        city: "bhopal",
+      },
+      cityState: "55",
+      address: "Near alpana cineplex hamidia road Bhopal",
+      locality: "hamidia road Bhopal",
+      parentId: 17529,
+      unserviceable: false,
+      veg: false,
+      select: false,
+      favorite: false,
+      tradeCampaignHeaders: [],
+      aggregatedDiscountInfo: {
+        header: "50% off",
+        shortDescriptionList: [
+          {
+            meta: "50% off | Use TRYNEW",
+            discountType: "Percentage",
+            operationType: "RESTAURANT",
+          },
+        ],
+        descriptionList: [
+          {
+            meta: "50% off up to ₹100 | Use code TRYNEW",
+            discountType: "Percentage",
+            operationType: "RESTAURANT",
+          },
+        ],
+        subHeader: "",
+        headerType: 0,
+        superFreedel: "",
+      },
+      aggregatedDiscountInfoV2: {
+        header: "50% OFF",
+        shortDescriptionList: [
+          {
+            meta: "Use TRYNEW",
+            discountType: "Percentage",
+            operationType: "RESTAURANT",
+          },
+        ],
+        descriptionList: [
+          {
+            meta: "50% off up to ₹100 | Use code TRYNEW",
+            discountType: "Percentage",
+            operationType: "RESTAURANT",
+          },
+        ],
+        subHeader: "",
+        headerType: 0,
+        superFreedel: "",
+      },
+      chain: [],
+      feeDetails: {
+        fees: [],
+        totalFees: 0,
+        message: "",
+        title: "",
+        amount: "",
+        icon: "",
+      },
+      availability: {
+        opened: true,
+        nextOpenMessage: "",
+        nextCloseMessage: "",
+      },
+      longDistanceEnabled: 0,
+      rainMode: "NONE",
+      thirdPartyAddress: false,
+      thirdPartyVendor: "",
+      adTrackingID: "",
+      badges: {
+        imageBased: [],
+        textBased: [],
+        textExtendedBadges: [],
+      },
+      lastMileTravelString: "2.2 kms",
+      hasSurge: false,
+      sla: {
+        restaurantId: "109939",
+        deliveryTime: 24,
+        minDeliveryTime: 24,
+        maxDeliveryTime: 24,
+        lastMileTravel: 2.200000047683716,
+        lastMileDistance: 0,
+        serviceability: "SERVICEABLE",
+        rainMode: "NONE",
+        longDistance: "NOT_LONG_DISTANCE",
+        preferentialService: false,
+        iconType: "EMPTY",
+      },
+      promoted: false,
+      avgRating: "3.9",
+      totalRatings: 10000,
+      new: false,
+    },
+    subtype: "basic",
+  },
+  {
+    type: "restaurant",
+    data: {
+      type: "F",
+      id: "428922",
+      name: "Mughal Darbar",
+      uuid: "d10f11ce-e3b2-49b9-b43b-b853abcef840",
+      city: "55",
+      area: "Hamidia Road",
+      totalRatingsString: "5000+ ratings",
+      cloudinaryImageId: "za3zbwlrxwuqtsji7jum",
+      cuisines: ["North Indian", "Mughlai", "Snacks", "Biryani"],
+      tags: [],
+      costForTwo: 30000,
+      costForTwoString: "₹300 FOR TWO",
+      deliveryTime: 21,
+      minDeliveryTime: 21,
+      maxDeliveryTime: 21,
+      slaString: "21 MINS",
+      lastMileTravel: 1.7999999523162842,
+      slugs: {
+        restaurant:
+          "mughal-darbar-family-restaurant-&-fast-food-jahangirabad-jahangirabad",
+        city: "bhopal",
+      },
+      cityState: "55",
+      address:
+        "62,Sangam Talkies Tiraha,Hamidia Road,Opposite Axis Bank,Bhopal 462001",
+      locality: "Jahangirabad",
+      parentId: 8892,
+      unserviceable: false,
+      veg: false,
+      select: false,
+      favorite: false,
+      tradeCampaignHeaders: [],
+      aggregatedDiscountInfo: {
+        header: "60% off",
+        shortDescriptionList: [
+          {
+            meta: "60% off | Use TRYNEW",
+            discountType: "Percentage",
+            operationType: "RESTAURANT",
+          },
+        ],
+        descriptionList: [
+          {
+            meta: "60% off up to ₹120 | Use code TRYNEW",
+            discountType: "Percentage",
+            operationType: "RESTAURANT",
+          },
+        ],
+        subHeader: "",
+        headerType: 0,
+        superFreedel: "",
+      },
+      aggregatedDiscountInfoV2: {
+        header: "60% OFF",
+        shortDescriptionList: [
+          {
+            meta: "Use TRYNEW",
+            discountType: "Percentage",
+            operationType: "RESTAURANT",
+          },
+        ],
+        descriptionList: [
+          {
+            meta: "60% off up to ₹120 | Use code TRYNEW",
+            discountType: "Percentage",
+            operationType: "RESTAURANT",
+          },
+        ],
+        subHeader: "",
+        headerType: 0,
+        superFreedel: "",
+      },
+      chain: [],
+      feeDetails: {
+        fees: [],
+        totalFees: 0,
+        message: "",
+        title: "",
+        amount: "",
+        icon: "",
+      },
+      availability: {
+        opened: true,
+        nextOpenMessage: "",
+        nextCloseMessage: "",
+      },
+      longDistanceEnabled: 0,
+      rainMode: "NONE",
+      thirdPartyAddress: false,
+      thirdPartyVendor: "",
+      adTrackingID: "",
+      badges: {
+        imageBased: [],
+        textBased: [],
+        textExtendedBadges: [],
+      },
+      lastMileTravelString: "1.7 kms",
+      hasSurge: false,
+      sla: {
+        restaurantId: "428922",
+        deliveryTime: 21,
+        minDeliveryTime: 21,
+        maxDeliveryTime: 21,
+        lastMileTravel: 1.7999999523162842,
+        lastMileDistance: 0,
+        serviceability: "SERVICEABLE",
+        rainMode: "NONE",
+        longDistance: "NOT_LONG_DISTANCE",
+        preferentialService: false,
+        iconType: "EMPTY",
+      },
+      promoted: false,
+      avgRating: "3.8",
+      totalRatings: 5000,
+      new: false,
+    },
+    subtype: "basic",
+  },
+];
+
+const RestrauntCard = ({ restaurant }) => {
+  const imgURL =
+    "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
   return (
     <div className="card">
-      <img src={restrauntData.img} />
-      <h2>{restrauntData.name}</h2>
-      <h3>{restrauntData.cousinName.join(", ")}</h3>
-      <h4>{restrauntData.rating} star</h4>
+      <img src={imgURL + restaurant.data?.cloudinaryImageId} />
+      <h2>{restaurant.data?.name}</h2>
+      {/* <h3>{restaurant.cousinName.join(", ")}</h3> */}
+      <h3>{restaurant.data?.cuisines.join(", ")}</h3>
+
+      <h4>{restaurant.data?.totalRatingsString} </h4>
     </div>
   );
 };
@@ -77,6 +474,11 @@ const RestrauntCard = () => {
 const Body = () => {
   return (
     <div className="restrauntlist">
+      <RestrauntCard restaurant={restrauntData[0]} />
+      <RestrauntCard restaurant={restrauntData[1]} />
+      <RestrauntCard restaurant={restrauntData[2]} />
+
+      {/* <RestrauntCard />
       <RestrauntCard />
       <RestrauntCard />
       <RestrauntCard />
@@ -84,11 +486,7 @@ const Body = () => {
       <RestrauntCard />
       <RestrauntCard />
       <RestrauntCard />
-      <RestrauntCard />
-      <RestrauntCard />
-      <RestrauntCard />
-      <RestrauntCard />
-      <RestrauntCard />
+      <RestrauntCard /> */}
     </div>
   );
 };
