636ce892b4d19b76dbbb26d201016f9de3ca5eb9
Nadeem <anwarnadeem2010@gmail.com>
2023-03-09 10:40:10 +0530

    Start working on useEffect

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,14 +1,72 @@
+import React, { useEffect, useState } from "react";
 import { restrauntData } from "../config";
 import RestrauntCard from "./RestrauntCard";
 
 const Body = () => {
+  //   const searchText = "biryani";
+  const [restaurants, setRestaurants] = useState(restrauntData);
+  const [searchText, setSearchText] = useState("");
+
+  // console.log("render() for sereach");
+
+  console.log("restaurants");
+
+  useEffect(() => {
+    // This place is best API Call
+    // console.log("render useeffect");
+    getRestrauntList();
+  }, []);
+
+  async function getRestrauntList() {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=23.2507549&lng=77.41568889999999&page_type=DESKTOP_WEB_LISTING"
+    );
+
+    const json = await data.json();
+    // console.log(json);
+    // console.log(json?.data?.cards[2]?.data?.data?.cards);
+    setRestaurants(json?.data?.cards[2]?.data?.data?.cards);
+  }
+
+  function filterRestraunt(searchText, restaurants) {
+    const filterData = restaurants.filter((restaurant) =>
+      restaurant.data.name.includes(searchText)
+    );
+    return filterData;
+  }
+
   return (
-    <div className="restrauntlist">
-      {restrauntData.map((restaurant) => {
-        return <RestrauntCard key={restaurant.data.id} {...restaurant.data} />;
-      })}
-    
-    </div>
+    <>
+      <div className="searchContainer">
+        <input
+          type="text"
+          value={searchText}
+          placeholder="search your food"
+          className="seach-input"
+          onChange={(e) => setSearchText(e.target.value)}
+        />
+        <button
+          className="search-btn"
+          onClick={() => {
+            //need to filter the data
+            const data = filterRestraunt(searchText, restaurants);
+            //after filter update the state
+
+            setRestaurants(data);
+          }}
+        >
+          Search
+        </button>
+        {/* {searchText} */}
+      </div>
+      <div className="restrauntlist">
+        {restaurants.map((restaurant) => {
+          return (
+            <RestrauntCard key={restaurant.data.id} {...restaurant.data} />
+          );
+        })}
+      </div>
+    </>
   );
 };
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,26 @@
+import { useState } from "react";
 import foodLogo from "../../foo-logo.jpg";
 
 const Title = () => (
   <a href="/">
-    <img
-      src={foodLogo}
-      className="logo"
-      alt="Logo"
-    />
+    <img src={foodLogo} className="logo" alt="Logo" />
   </a>
 );
 
 const Header = () => {
+  // let title = "Food Villa";
+
+  const [title, setTitle] = useState("Food Villa");
+
+  console.log("render()");
+
   return (
     <div className="header">
       <Title />
+
+      <h1> {title}</h1>
+
+      <button onClick={() => setTitle("New title ")}>Change title</button>
 
       <div className="nav-items">
         <ul>
