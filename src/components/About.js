2b334bf8ed82c4326daf18804ca417221843b4fe
Nadeem <anwarnadeem2010@gmail.com>
2023-03-17 12:31:36 +0530

    state life cycle working

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,11 @@
 // import { Outlet, Link } from "react-router-dom";
-
+import React from "react";
 import { Outlet, Link } from "react-router-dom";
 import Profile from "./Profile";
 import ProfileClass from "./ProfileClass";
+import { Component } from "react";
 
-const About = () => {
+const About1 = () => {
   return (
     <div>
       <h1>About Us page</h1>
@@ -22,5 +23,52 @@ const About = () => {
     </div>
   );
 };
+
+class About extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      // name: "github user",
+      // userPic: "demo pic",
+    };
+
+    console.log(" parent- constructor");
+  }
+
+  componentDidMount() {
+    // best palce for api call
+    // const url = "https://api.github.com/users/nadeem-ds";
+    // const data = await fetch(url);
+    // const responce = await data.json();
+    // this.setState({ name: responce.name, userPic: responce.avatar_url });
+    // console.log(responce);
+    // console.log(responce.name);
+    console.log(" parent- component did mount");
+  }
+
+  render() {
+    console.log(" parent- render");
+    
+    return (
+      <div>
+        <h1>About Us page</h1>
+        {/* <h1>Name:{this.state.name}</h1>
+        <img src={this.state.userPic} alt="demo pic" /> */}
+
+        {/* function component */}
+
+        {/* <Outlet />
+
+      <Link to="profile">
+        <button>Auhor page </button>
+      </Link> */}
+
+        {/* <Profile /> */}
+        <ProfileClass name={"Nadeem "} />
+      </div>
+    );
+  }
+}
 
 export default About;
--- src/components/ProfileClass.js
+++ src/components/ProfileClass.js
@@ -1,41 +1,68 @@
 import React from "react";
+import { Component } from "react";
 
-class ProfileClass extends React.Component {
+class ProfileClass extends Component {
   constructor(props) {
     super(props);
     //this is the place we initilize the state
 
+    // this.state = {
+    //   count: 0,
+    //   count2: 0,
+    // };
+
     this.state = {
-      count: 0,
-      count2: 0,
+      name: "github user",
+      userPic: "demo pic",
     };
 
-    console.log("constructor");
+    console.log(" child-constructor");
   }
 
-  componentDidMount() {
-    // it will render after render
-    // 1. constructor
-    // 2.render
-    // 3. componentDidMount
+  // componentDidMount() {
+  // it will render after render
+  // 1. constructor
+  // 2.render
+  // 3. componentDidMount
 
+  //THis is the place for api call
 
-    //THis is the place for api call
+  //   console.log("  child- component did mount");
+  // }
 
-    
+  async componentDidMount() {
+    // best palce for api call
 
-    console.log("component did mount");
+    const url = "https://api.github.com/users/chetannada";
+    const data = await fetch(url);
+    const responce = await data.json();
+    this.setState({ name: responce.name, userPic: responce.avatar_url });
+    console.log(responce);
+    console.log(responce.name);
+
+    console.log(" parent- component did mount");
+
+    console.log(this.state.name);
+    console.log(this.state.userPic);
+  }
+
+  componentDidUpdate() {
+    console.log("componet did update");
+  }
+
+  componentWillUnmount() {
+    console.log("component destroy");
   }
 
   render() {
-    console.log("render");
+    console.log(" child- render");
 
     return (
       <div>
         <h1>This is class component</h1>
-        <h2>Name :{this.props.name}</h2>
+        {/* <h2>Name :{this.props.name}</h2> */}
         {/* use the state variabl */}
-        <h3>Count : {this.state.count}</h3>
+        {/* <h3>Count : {this.state.count}</h3>
         <h3>Count : {this.state.count2}</h3>
         <button
           onClick={() => {
@@ -46,7 +73,11 @@ class ProfileClass extends React.Component {
           }}
         >
           CountInc
-        </button>
+        </button> */}
+
+        <h1>About Us page</h1>
+        <h1>Name:{this.state.name}</h1>
+        <img src={this.state.userPic} alt="demo pic" />
       </div>
     );
   }
