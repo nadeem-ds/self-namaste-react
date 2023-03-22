a0c0e23234a4b14d3779163333c275347a0421c1
Nadeem <anwarnadeem2010@gmail.com>
2023-03-23 00:20:38 +0530

    props lifting

--- src/components/Instamart.js
+++ src/components/Instamart.js
@@ -1,13 +1,14 @@
 import React, { useState } from "react";
 
-const Section = ({ title, desc }) => {
-  const [show, setShow] = useState(false);
+const Section = ({ title, desc, show, setVisible }) => {
+  // const [show, setShow] = useState(false);
 
   return (
     <div className="border border-black p-3 m-4 rounded-md">
       <h3 className="font-bold text-xl m-2">{title}</h3>
       <button
-        onClick={() => setShow(!show)}
+        onClick={() => setVisible()}
+        // onClick={onClick}
         className="border border-black p-2 rounded-md "
       >
         {/* Show desc */}
@@ -20,6 +21,10 @@ const Section = ({ title, desc }) => {
 };
 
 const Instamart = () => {
+  const [sectionConfig, setSectionConfig] = useState({
+    showProduct: false,
+    showTeam: false,
+  });
   return (
     <div>
       <h2 className="font-bold text-3xl p-3 m-4">About Instamart</h2>
@@ -28,11 +33,25 @@ const Instamart = () => {
         desc={
           "On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains"
         }
+        show={sectionConfig.showProduct}
+        setVisible={() => {
+          setSectionConfig({
+            showProduct: true,
+            showTeam: false,
+          });
+        }}
       />
       <Section
         title={"Team  "}
         desc={
           "On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains"
+        }
+        show={sectionConfig.showTeam}
+        setVisible={() =>
+          setSectionConfig({
+            showProduct: false,
+            showTeam: true,
+          })
         }
       />
     </div>
