b25f5e8cce1fb6d763bf79557c66b87e8cc33647
Nadeem <anwarnadeem2010@gmail.com>
2023-03-23 12:48:14 +0530

    usecontext in our app

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,12 @@
+import React, { useContext } from "react";
+import Usercontext from "../utils/UserContext";
 const Footer = () => {
+  const { user } = useContext(Usercontext);
   return (
-    <div>
-      <h1>Footer</h1>
+    <div className="p-3 m-3 bg-pink-600 rounded-lg">
+      <h1 className="font-bold text-2xl text-white">
+        This site is developed by {user.name} - {user.email}
+      </h1>
     </div>
   );
 };
--- src/components/RestrauntCard.js
+++ src/components/RestrauntCard.js
@@ -17,7 +17,7 @@ const RestrauntCard = ({
   return (
     // <h4>{nameuser}</h4>
 
-    <div className=" w-56 h-80 p-2 m-5 flex flex-col border-2  flex-wrap shadow-md  bg-pink-100">
+    <div className=" w-56 h-90 p-2 m-5 flex flex-col border-2  flex-wrap shadow-md  bg-pink-100 overflow-hidden">
       <img className="img-card" src={IMG_CDN + cloudinaryImageId} />
       <h2 className="font-bold text-xl mt-4">{name}</h2>
       {/* <h3>{cousinName.join(", ")}</h3> */}
@@ -25,6 +25,7 @@ const RestrauntCard = ({
 
       <h4>{totalRatingsString} </h4>
       <h3 className="font-bold">{user.name}</h3>
+      <h3 className="font-bold">{user.email}</h3>
       {/* <h3>{user.name}</h3> */}
     </div>
   );
