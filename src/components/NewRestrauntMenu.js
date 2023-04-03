1c1f6d2bc8541758f264d1de9aa916bf3ff2133d
Nadeem <anwarnadeem2010@gmail.com>
2023-04-03 12:59:55 +0530

    show res menu bug fix

--- src/components/NewRestrauntMenu.js
+++ src/components/NewRestrauntMenu.js
@@ -5,70 +5,163 @@ import { IMG_CDN } from "../config";
 import useRestrauntMenu from "../utils/useRestrauntMenu";
 import Shimmer from "./Shimmer";
 const NewRestrauntMenu = () => {
-  // const [menuData, setMenuData] = useState(null);
+  const [menuData, setMenuData] = useState(null);
 
+  // var apifinalres = null;
   const params = useParams();
   const { id } = params;
+  console.log(id);
+  const API_URL =
+    "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9715987&lng=77.5945627&restaurantId=" +
+    id;
 
-  const menuData = useRestrauntMenu(id);
+  console.log(API_URL);
 
-  return !menuData ? (
-    <Shimmer />
-  ) : (
-    <div className="restraunt-menu-list">
-      <div className="MainRestraunt">
-        <img src={IMG_CDN + menuData?.cloudinaryImageId} alt="resPic" />
-        <h1>{menuData?.name}</h1>
-        <h1>The restaurant id : {id}</h1>
-        <h2>{menuData?.area}</h2>
-        <h2>{menuData?.city}</h2>
-        <h3>{menuData?.avgRating} star</h3>
-        <h3>{menuData?.costForTwoMsg}</h3>
-      </div>
+  // const menuData = useRestrauntMenu(id);
 
-      <div>
-        <h1>Menu</h1>
-        {Object.values(menuData?.menu?.items).map((data) => (
-          <li key={data.id}>{data.name}</li>
-        ))}
-        {/* <div> {menuData?.menu?.items}</div> */}
+  // return !menuData ? (
+  //   <Shimmer />
+  // ) : (
+  //   <div className="restraunt-menu-list">
+  //     <div className="MainRestraunt">
+  //       <h1>THis is res menu section</h1>
+  //       <img src={IMG_CDN + menuData?.cloudinaryImageId} alt="resPic" />
+  //       <h1>{menuData?.name}</h1>
+  //       <h1>The restaurant id : {id}</h1>
+  //       <h2>{menuData?.area}</h2>
+  //       <h2>{menuData?.city}</h2>
+  //       <h3>{menuData?.avgRating} star</h3>
+  //       <h3>{menuData?.costForTwoMsg}</h3>
+  //     </div>
 
-        {/* {menuData.map((data) => {
-          <li>{data}</li>;
-        })} */}
+  //     <div>
+  //       <h1>Menu</h1>
+  //       {Object.values(menuData?.menu?.items).map((data) => (
+  //         <li key={data.id}>{data.name}</li>
+  //       ))}
+  //       {/* <div> {menuData?.menu?.items}</div> */}
 
-        {/* <p>
-          {menuListData.map((data) => (
-            <li>{data}</li>
-          ))}
-        </p> */}
-        <ul>
-          {/* {Object.values(menuData.menu.items).map((item) => (
-            <li key={item.id}>{item.name}</li>
-          ))} */}
+  //       {/* {menuData.map((data) => {
+  //         <li>{data}</li>;
+  //       })} */}
 
-          {/* new api call */}
-          {/* {Object.values(menuData.menu.item).map((item) => (
-            <li key={item.id}>{item.name}</li>
-          ))} */}
+  //       {/* <p>
+  //         {menuListData.map((data) => (
+  //           <li>{data}</li>
+  //         ))}
+  //       </p> */}
+  //       <ul>
+  //         {/* {Object.values(menuData.menu.items).map((item) => (
+  //           <li key={item.id}>{item.name}</li>
+  //         ))} */}
 
-          {/* {Object.values(menuData?.menu?.items).map((data) => (
-            <li>{data.name}</li>
-          ))} */}
-        </ul>
-        <ul>
-          {/* {Object.values(menuDish?.cards[1]?.card?.card?.itemCards).map(
-            (item) => (
-              <li key={item.card.info.id}>{item.info.name}</li>
-            )
-          )} */}
-        </ul>
-        <ul>
-          {/* {Object.values(menuData?.menu?.items).map((data, key) => (
-            <li key={data.id}>{data.name}</li>
-          ))} */}
-        </ul>
-      </div>
+  //         {/* new api call */}
+  //         {/* {Object.values(menuData.menu.item).map((item) => (
+  //           <li key={item.id}>{item.name}</li>
+  //         ))} */}
+
+  //         {/* {Object.values(menuData?.menu?.items).map((data) => (
+  //           <li>{data.name}</li>
+  //         ))} */}
+  //       </ul>
+  //       <ul>
+  //         {/* {Object.values(menuDish?.cards[1]?.card?.card?.itemCards).map(
+  //           (item) => (
+  //             <li key={item.card.info.id}>{item.info.name}</li>
+  //           )
+  //         )} */}
+  //       </ul>
+  //       <ul>
+  //         {/* {Object.values(menuData?.menu?.items).map((data, key) => (
+  //           <li key={data.id}>{data.name}</li>
+  //         ))} */}
+  //       </ul>
+  //     </div>
+  //   </div>
+  // );
+
+  useEffect(() => {
+    async function getMenudata() {
+      const menuDataFetch = await fetch(API_URL);
+      const data = await menuDataFetch.json();
+      console.log("Api data", data);
+
+      setMenuData(
+        data.data.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card
+          ?.card?.itemCards
+      );
+
+      // console.log("DATA", apifinalres);
+
+      console.log(
+        "Full api data",
+        data.data.cards[2].groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card
+          ?.card?.itemCards
+      );
+
+      console.log(
+        "one item",
+        data.data.cards[2].groupedCard.cardGroupMap.REGULAR.cards[2].card.card
+          .itemCards[1].card.info.name
+      );
+
+      console.log(
+        "type check",
+        typeof data.data.cards[2].groupedCard?.cardGroupMap?.REGULAR?.cards[1]
+          ?.card?.card?.itemCards
+      );
+
+      //it is working now
+      // {
+      //   data.data.cards[2].groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card?.itemCards.map(
+      //     (item) => console.log(item.card.info.name)
+      //   );
+      // }
+
+      console.log("Menu data", menuData);
+
+      // setMenuData(apifinalres);
+      // console.log(
+      //   "DATA",
+      //   data?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card
+      //     .itemCards
+      // );
+    }
+    getMenudata();
+  }, []);
+  return (
+    <div>
+      <h1>Hotel menu data</h1>
+
+      {console.log("OBJECT CONVERT", Object.values(menuData))}
+
+      {Object.values(menuData).map((item) => (
+        <li
+          className="bg-gray-300 m-3 p-2 rounded-lg list-none"
+          key={item.card.info.id}
+        >
+          {item.card.info.name}
+        </li>
+      ))}
+
+      {/* {Object.values(menuData?.card).map((item) => console.log(item))} */}
+
+      {/* {Object.values(
+        menuData.map(({ card }) => {
+          <li>{card}</li>;
+        })
+      )} */}
+      {/* {menuData.map((item) => {
+        console.log(item.itemCards.card.info.name);
+      })} */}
+      {/* {menuData.map((item) => (
+        <li>{item}</li>
+      ))} */}
+      {/* <h2>{menuData}</h2> */}
+      {/* {console.log(typeof apifinalres)}
+      {Object.values(apifinalres).map((item) => (
+        <li>{item.card.info.name}</li>
+      ))} */}
     </div>
   );
 };
--- src/utils/useRestrauntMenu.js
+++ src/utils/useRestrauntMenu.js
@@ -1,45 +1,21 @@
 import React, { useState, useEffect } from "react";
-// import { GET_RESTAURANT_MENU } from "../config";
 
 import { GET_RESTAURANT_MENU } from "../config";
 const useRestrauntMenu = (id) => {
   const [menuData, setMenuData] = useState(null);
 
   console.log("id", id);
-  //first fetch the data from server
-  const URL =
-    "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=23.2507549&lng=77.41568889999999&restaurantId=" +
-    id;
 
-  const URL1 =
-    "https://www.swiggy.com/dapi/menu/v4/full?lat=27.8973944&lng=78.0880129&menuId=" +
-    id;
-
-  const url = `https://www.swiggy.com/dapi/menu/v4/full?lat=27.8973944&lng=78.0880129&menuId=${id}`;
-  const newurl =
-    "https://www.swiggy.com/dapi/menu/v4/full?lat=12.9351929&lng=77.62448069999999&menuId=" +
-    { id };
-  console.log("res menu data api url", url);
-
-  //   const URL =
-  //     "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=23.2507549&lng=77.41568889999999&restaurantId=107521";
-  // console.log(GET_RESTAURANT_MENU + id);
-
-  // console.log(GET_RESTAURANT_MENU);
-  const burl = GET_RESTAURANT_MENU + id;
-  console.log("burl", burl);
   useEffect(() => {
+    async function getRestrauntMenu() {
+      const data = await fetch(
+        " https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9715987&lng=77.5945627&restaurantId=3241#"
+      );
+      const json = await data.json();
+      console.log("Menu data ", json.data);
+    }
     getRestrauntMenu();
   }, []);
-
-  async function getRestrauntMenu() {
-    const data = await fetch(burl);
-    const json = await data.json();
-
-    console.log("Menu data ", json.data);
-    setMenuData(json?.data);
-  }
-  //return the data
   return menuData;
 };
 
